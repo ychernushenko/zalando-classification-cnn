bbf081b78393cbab1caf6c57c13b04b0717b67a1
Yury Chernushenko <y.chernushenko@gmail.com>
2017-09-07 09:38:50 +0200

    next

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 
 TEXT_DATA_DIR = '20_newsgroup'
+GLOVE_DIR = 'glove.6B'
 MAX_NB_WORDS = 20000
 MAX_SEQUENCE_LENGTH = 1000
 VALIDATION_SPLIT = 0.2
+EMBEDDING_DIM = 100
 
 texts = []  # list of text samples
 labels_index = {}  # dictionary mapping label name to numeric id
@@ -55,3 +57,21 @@ x_train = data[:-nb_validation_samples]
 y_train = labels[:-nb_validation_samples]
 x_val = data[-nb_validation_samples:]
 y_val = labels[-nb_validation_samples:]
+
+embeddings_index = {}
+f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
